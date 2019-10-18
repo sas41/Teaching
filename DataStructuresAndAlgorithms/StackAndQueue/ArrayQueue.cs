8a43fb68e092f9f9e43cf642bfc4cc6ac897a15a
Berk A <sas41@users.noreply.github.com>
2019-10-18 09:45:27 +0300

    Added Queue Implementation

---
+++ DataStructuresAndAlgorithms/StackAndQueue/ArrayQueue.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    class ArrayQueue<T>
+    {
+        const int array_size = 16;
+        T[] ts = new T[array_size];
+        int head = 0;
+        int tail = 0;
+        public ArrayQueue()
+        {
+
+        }
+
+        public void Enqueue(T item)
+        {
+            if (tail < array_size)
+            {
+                ts[tail] = item;
+                tail++;
+            }
+            else
+            {
+                throw new StackOverflowException("Queue Full.");
+            }
+        }
+
+        public T Dequeue()
+        {
+            if (head != tail)
+            {
+                T item = ts[head];
+                ts[head] = default(T);
+
+                head++;
+
+                if (head == tail)
+                {
+                    head = 0;
+                    tail = 0;
+                }
+
+                return item;
+            }
+            else
+            {
+                throw new InvalidOperationException("Queue Empty.");
+            }
+        }
+
+        public T Peek()
+        {
+            if (head != tail)
+            {
+                return ts[head];
+            }
+            else
+            {
+                throw new InvalidOperationException("Queue Empty.");
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return head == tail;
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/StackAndQueue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> myQueue = new Queue<int>();
+            int total = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                myQueue.Enqueue(input);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                total = total + myQueue.Peek();
+                Console.WriteLine(myQueue.Dequeue());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(total);
+        }
+    }
+}
