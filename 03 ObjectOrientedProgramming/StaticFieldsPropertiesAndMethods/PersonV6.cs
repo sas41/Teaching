e410cac7cecb7e479a1e1197c68843aec2a539e6
Berk A <sas41@users.noreply.github.com>
2020-05-21 11:05:04 +0300

    Static Fields, Properties and Methods

---
+++ 03 ObjectOrientedProgramming/StaticFieldsPropertiesAndMethods/PersonV6.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticFieldsPropertiesAndMethods
+{
+
+    class PersonV6
+    {
+        static private int count;
+
+        static public int Count
+        {
+            get { return count; }
+            private set { count = value; }
+        }
+
+        static public List<PersonV6> People { get; private set; }
+
+
+        static PersonV6()
+        {
+            People = new List<PersonV6>();
+        }
+
+
+
+        private string firstName;
+        private string lastName;
+        private int age;
+        private string ssn;
+        private string secret;
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                if (value.Length > 1)
+                {
+                    this.firstName = value;
+                }
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                if (value.Length > 1)
+                {
+                    this.lastName = value;
+                }
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    this.age = value;
+                }
+            }
+        }
+        public string SSN
+        {
+            get
+            {
+                return this.ssn;
+            }
+            set
+            {
+                if (value.Length == 10)
+                {
+                    this.ssn = value;
+                }
+            }
+        }
+
+        public PersonV6()
+        {
+            this.FirstName = "N/A";
+            this.LastName = "N/A";
+            this.Age = 0;
+            this.SSN = "----------";
+
+            Count++;
+            People.Add(this);
+        }
+
+        public PersonV6(string firstName, string lastName, int age, string ssn)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.SSN = ssn;
+
+            Count++;
+            People.Add(this);
+        }
+
+        public PersonV6(string fullname, int age, string ssn) : this(fullname.Split()[0], fullname.Split()[1], age, ssn)
+        {
+        }
+
+
+
+        string FullName()
+        {
+            return $"{this.FirstName} {this.LastName}";
+        }
+
+        public void Present()
+        {
+            Console.WriteLine($"Hello, my name is {this.FullName()}");
+            Console.WriteLine($"I am {this.Age} years old.");
+            Console.WriteLine($"My social security number is: {this.SSN}");
+        }
+    }
+}
---
+++ 03 ObjectOrientedProgramming/StaticFieldsPropertiesAndMethods/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticFieldsPropertiesAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int test = int.Parse("199");
+
+
+            PersonV6 test = new PersonV6("Gosho", "Georgiev", 29, "9101295057");
+            Console.WriteLine(PersonV6.Count);
+
+            test = new PersonV6("Pesho Petrov", 21, "9903127078");
+            Console.WriteLine(PersonV6.Count);
+
+            test = new PersonV6();
+            Console.WriteLine(PersonV6.Count);
+
+
+
+            foreach (var person in PersonV6.People)
+            {
+                person.Present();
+                Console.WriteLine();
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
